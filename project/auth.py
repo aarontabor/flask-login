79f8a34c2cc42cf4026dc7cc906bed2be4db3301
Aaron Tabor <aarontabor50@gmail.com>
2021-02-18 14:26:48 -0400

    Add signup logic.

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash
+
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -11,6 +14,24 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    # Email addresses can only be used for a single account
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
