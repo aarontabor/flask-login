3de706e26c7069529a7bbb2e7ac807de00de0a29
Aaron Tabor <aarontabor50@gmail.com>
2021-02-18 14:46:48 -0400

    Protect profile page behind user authentication.

--- project/main.py
+++ project/main.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
+
 from . import db
 
 main = Blueprint('main', __name__)
@@ -8,5 +10,6 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html')
